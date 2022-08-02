d8e0a77b272b8f01e498c7a61fe4ee408f4155d4
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-02 19:31:12 +0300

    add homework for lesson04

--- script.js
+++ script.js
@@ -1,48 +1,15 @@
-let lang = 'ru';
-const rusDayOfWeek = [
-  'Понедельник',
-  'Вторник',
-  'Среда',
-  'Четверг',
-  'Пятницу',
-  'Суббота',
-  'Воскресенье',
-];
-const enDayOfWeek = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
-const allDayOfWeek = [rusDayOfWeek, enDayOfWeek];
-console.log('allDayOfWeek: ', allDayOfWeek);
+const strOperation = (str) => {
+  if (typeof str !== 'string') {
+    alert('Передана не строка');
+    return;
+  }
 
-// if
-if (lang === 'ru') {
-  console.log(rusDayOfWeek);
-} else if (lang === 'en') {
-  console.log(enDayOfWeek);
-}
-// switch
-switch (lang) {
-  case 'ru':
-    console.log(rusDayOfWeek);
-    break;
-  case 'en':
-    console.log(enDayOfWeek);
-    break;
-}
-//
-lang === 'ru'
-  ? console.log(allDayOfWeek[0])
-  : console.log(allDayOfWeek[1]);
+  str.replace(/ /g, '').slice(0, 30);
+  if (str.length > 30) {
+    str += '...';
+  }
 
-let namePerson = 'Александр';
+  return str;
+};
 
-namePerson === 'Артём' ? console.log('директор') : console.log('студент');
-namePerson === 'Александр'
-  ? console.log('преподаватель')
-  : console.log('студент');
+console.log(strOperation('123456789123456789123456789123456789'));
