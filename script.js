d19051f047827f6509f1863f188acffa2520fc37
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-03 18:43:08 +0300

    fix homework

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const strOperation = (str) => {
   if (typeof str !== 'string') {
-    alert('Передана не строка');
+    console.log('Передана не строка');
     return;
   }
-
-  str.replace(/ /g, '').slice(0, 30);
+  
   if (str.length > 30) {
+    str = str.trim().slice(0, 30);
     str += '...';
   }
 
   return str;
 };
 
-console.log(strOperation('123456789123456789123456789123456789'));
+console.log(strOperation('        123456789123456789123456789123456789'));
