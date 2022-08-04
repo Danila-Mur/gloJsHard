1cce10c9f934fe0dcb9750e6b7022dbc00eaec5b
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-04 11:47:30 +0300

    add homework lesson05

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-const strOperation = (str) => {
-  if (typeof str !== 'string') {
-    console.log('Передана не строка');
-    return;
-  }
-  
-  if (str.length > 30) {
-    str = str.trim().slice(0, 30);
-    str += '...';
-  }
+// 1
+const arr = ['123456', '242376', '241124', '7412421', '894129', '7881294', '32412'];
+console.log('arr: ', arr);
 
-  return str;
-};
+arr.find((el) => {
+  if (el[0] === '2' || el[0] === '7') {
+    console.log(el);
+  }
+})
 
-console.log(strOperation('        123456789123456789123456789123456789'));
+//
+const num = 100;
+for (let i = 1; i <= num; i++) {
+  for (let j = 2; j <= i; j++) {
+    if (i % j === 0 && j < i) {
+      break;
+    } else if (j === i) {
+      console.log(i, 'Делители этого числа 1 и ' + i );
+    }
+  }
+}
