f8ee7199732f0a7db07c62b6303cec5fcc3a54d6
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-08 02:06:38 +0300

    add homework for lesson07

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-// 1
-const arr = ['123456', '242376', '241124', '7412421', '894129', '7881294', '32412'];
-console.log('arr: ', arr);
+const week = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ],
+  weekList = document.querySelector('.week__list');
 
-arr.find((el) => {
-  if (el[0] === '2' || el[0] === '7') {
-    console.log(el);
-  }
-})
+for (let key of week) {
+  weekList.innerHTML += `<li>${key}</li>`;
 
-//
-const num = 100;
-for (let i = 1; i <= num; i++) {
-  for (let j = 2; j <= i; j++) {
-    if (i % j === 0 && j < i) {
-      break;
-    } else if (j === i) {
-      console.log(i, 'Делители этого числа 1 и ' + i );
+  const weekItem = weekList.querySelectorAll('li');
+  const dayOfWeekName = new Date().toLocaleString('en-US', {
+    weekday: 'long',
+  });
+  weekItem.forEach((item) => {
+    switch (true) {
+      case item.textContent === 'Saturday' || item.textContent === 'Sunday':
+        item.style.fontStyle = 'italic';
+        return;
+      case dayOfWeekName === item.textContent:
+        item.style.fontWeight = 'bold';
+        return;
+      case (item.textContent === 'Saturday' || item.textContent === 'Sunday') &&
+        dayOfWeekName === item.textContent:
+        item.style.fontStyle = 'italic';
+        item.style.fontWeight = 'bold';
+        return;
     }
-  }
+  });
 }
